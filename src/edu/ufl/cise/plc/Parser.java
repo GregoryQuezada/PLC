aa9a6cebb5dca27360eaaa4365c8d0fc5e95b3d9
Gregory Quezada <quezada.gregory@yahoo.com>
2022-03-03 22:39:19 -0500

    All Grammar functions added without testing

--- src/edu/ufl/cise/plc/Parser.java
+++ src/edu/ufl/cise/plc/Parser.java
@@ -2,6 +2,8 @@ package edu.ufl.cise.plc;
 
 import edu.ufl.cise.plc.ast.*;
 
+import java.util.ArrayList;
+
 public class Parser implements IParser{
 
     private ILexer lexer;
@@ -29,6 +31,92 @@ public class Parser implements IParser{
             }
         }
         return false;
+    }
+
+    public Program program() throws PLCException {
+        IToken firstToken = t;
+        IToken name = null;
+        Program p = null;
+
+        if (isKind(IToken.Kind.TYPE, IToken.Kind.KW_VOID)) {
+            ArrayList<NameDef> nameDefList = new ArrayList<NameDef>();
+            ArrayList<ASTNode> ASTNodeList = new ArrayList<ASTNode>();
+
+            Types.Type returnType = Types.Type.toType(t.getText());
+            consume();
+            name = t;
+            consume();
+            match(IToken.Kind.LPAREN);
+
+            if (isKind(IToken.Kind.TYPE)) {
+                nameDefList.add(nameDef());
+
+                while (isKind(IToken.Kind.COMMA)) {
+                    consume();
+                    nameDefList.add(nameDef());
+                }
+            }
+
+            match(IToken.Kind.RPAREN);
+
+
+            while (isKind(IToken.Kind.TYPE, IToken.Kind.IDENT, IToken.Kind.KW_WRITE, IToken.Kind.RETURN)) {
+
+                if (isKind(IToken.Kind.TYPE)) {
+                    ASTNodeList.add(declaration());
+                } else {
+                    ASTNodeList.add(statement());
+                }
+                match(IToken.Kind.SEMI);
+            }
+
+            p = new Program(firstToken, returnType, name.getText(), nameDefList, ASTNodeList);
+
+        }
+        else {
+            throw new SyntaxException("Expected TYPE or VOID in program");
+        }
+
+        return p;
+    }
+
+    NameDef nameDef() throws PLCException {
+        IToken firstToken = t;
+        IToken type = null;
+        IToken name = null;
+        Dimension dim = null;
+
+        if (isKind(Token.Kind.TYPE)) {
+            type = t;
+            consume();
+            if (isKind(Token.Kind.LSQUARE)) {
+                dim = dimension();
+                name = t;
+                consume();
+                return new NameDefWithDim(firstToken, type, name, dim);
+            }
+            name = t;
+            consume();
+            return new NameDef(firstToken, type, name);
+        } else {
+            throw new SyntaxException("Expected TYPE in nameDef");
+        }
+
+    }
+
+    VarDeclaration declaration() throws PLCException {
+        IToken firstToken = t;
+        NameDef nameDef = nameDef();
+        IToken op = null;
+        Expr e = null;
+
+        if (isKind(IToken.Kind.ASSIGN, IToken.Kind.LARROW)) {
+            op = t;
+            consume();
+            e = expr();
+        }
+
+        return new VarDeclaration(firstToken, nameDef, op, e);
     }
 
     public Expr expr() throws PLCException {
@@ -222,6 +310,25 @@ public class Parser implements IParser{
             e = expr();
             match(IToken.Kind.RPAREN);
         }
+        else if (isKind(IToken.Kind.COLOR_CONST)) {
+            e = new ColorConstExpr(firstToken);
+            consume();
+        }
+        else if (isKind(IToken.Kind.LANGLE)) {
+            consume();
+            Expr e1 = expr();
+            match(IToken.Kind.COMMA);
+            Expr e2 = expr();
+            match(IToken.Kind.COMMA);
+            Expr e3 = expr();
+            match(IToken.Kind.RANGLE);
+            e = new ColorExpr(firstToken, e1, e2, e3);
+
+        }
+        else if (isKind(IToken.Kind.KW_CONSOLE)) {
+            e = new ConsoleExpr(firstToken);
+            consume();
+        }
         else {
             throw new SyntaxException("PrimaryExpr did not work");
         }
@@ -248,6 +355,77 @@ public class Parser implements IParser{
         }
 
         return e;
+    }
+
+    Dimension dimension() throws PLCException {
+        IToken firstToken = t;
+        Dimension e = null;
+        Expr left = null;
+        Expr right = null;
+
+        if (isKind(IToken.Kind.LSQUARE)){
+            consume();
+            left = expr();
+            match(IToken.Kind.COMMA);
+            right = expr();
+            match(IToken.Kind.RSQUARE);
+            e = new Dimension(firstToken, left, right);
+
+        }
+        else {
+            throw new SyntaxException("Expected LSQUARE in dimension");
+        }
+
+        return e;
+    }
+
+    Statement statement() throws PLCException {
+        IToken firstToken = t;
+        IToken str = null;
+        PixelSelector p = null;
+        IToken op = null;
+        Expr e = null;
+
+
+        if (isKind(IToken.Kind.IDENT)) {
+            str = t;
+
+            if (isKind(IToken.Kind.LSQUARE)) {
+                p = pixelSelector();
+            }
+
+            if (isKind(IToken.Kind.ASSIGN, IToken.Kind.LARROW)) {
+                op = t;
+                consume();
+                e = expr();
+
+                if (op.getKind() == IToken.Kind.ASSIGN) {
+                    return new AssignmentStatement(firstToken, str.getText(), p, e);
+                }
+                else {
+                    return new ReadStatement(firstToken, str.getText(), p, e);
+                }
+            }
+            else {
+                throw new SyntaxException("Expected ASSIGN or LARROW in statement");
+            }
+        }
+        else if (isKind(IToken.Kind.KW_WRITE)) {
+            consume();
+            e = expr();
+            match(IToken.Kind.RARROW);
+            Expr dest = expr();
+            return new WriteStatement(firstToken, e, dest);
+        }
+        else if (isKind(IToken.Kind.RETURN)) {
+            consume();
+            e = expr();
+            return new ReturnStatement(firstToken, e);
+        }
+        else {
+            throw new SyntaxException("Expected IDENT, WRITE or RETURN in statement");
+        }
+
     }
 
     private void match (IToken.Kind... types) throws PLCException {
